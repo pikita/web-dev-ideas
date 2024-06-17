a32ece3a16755121415c6da56fa8b280bd19fe54
massi <cadeddu.massi88@gmail.com>
2024-06-17 23:15:09 +0200

    "nuove prove"

---
+++ prove wrapper/script.js
@@ -0,0 +1,10 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slides');
+
+function showNextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+setInterval(showNextSlide, 3000);
